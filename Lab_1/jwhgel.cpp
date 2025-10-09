9b04bbe66130121809e8271f2bea4771cb933105
jwhgel <maria-akimovva@yandex.ru>
2025-10-09 09:21:25 +0300

    Add files via upload

---
+++ Lab_1/jwhgel.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    float x, y;
+
+    cin >> x;
+    cin >> y;
+
+    float S = tan(x / y);
+
+    float R = (pow(x, 1/3) * sin(x)) / (exp(3 * x) + exp(3 * y));
+
+    cout << "R = " << R << endl;
+    cout << "S = " << S << endl;
+
+    float C = max(R, S);
+
+    cout << "C = " << C << endl;
+
+    return 0;
+}
