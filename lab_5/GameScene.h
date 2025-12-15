e89d843a409f47cd6e491bbc970d94458b0d7b11
jwhgel <maria-akimovva@yandex.ru>
2025-12-16 01:25:51 +0300

    Add files via upload

---
+++ lab_5/GameScene.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "TmxLevel.h"
+#include <vector>
+#include <SFML/System.hpp>
+
+struct GameView;
+
+struct GameScene
+{
+    TmxLevel level;
+    TmxObject player;
+    std::vector<TmxObject> enemies;   // âðàãè
+    std::vector<TmxObject> coins;     // ìîíåòû
+    std::vector<TmxObject> blocks;    // ïðåïÿòñòâèÿ
+    sf::Vector2f startPosition;       // íà÷àëüíàÿ ïîçèöèÿ èãðîêà
+};
+
+GameScene* NewGameScene();
+void UpdateGameScene(void* pData, GameView& view, float deltaSec);
+void DrawGameScene(void* pData, GameView& view);
+void DestroyGameScene(GameScene*& pScene);
